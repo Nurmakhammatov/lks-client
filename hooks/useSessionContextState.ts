63e4d923fd337bd29e9104131aafd6aa669379a3
Shukhratullo <shuhratillon@gmail.com>
2024-02-03 16:39:03 +0500

    Added build step to CI & Removed TODO's

--- hooks/useSessionContextState.ts
+++ hooks/useSessionContextState.ts
@@ -1,7 +1,7 @@
 import type { SessionContextState } from 'types/contexts/session';
 
 const useSessionContextState = (): SessionContextState => ({
-  theme: undefined // TODO: Load user theme from LocalStorage
+  theme: undefined
 });
 
 export default useSessionContextState;
