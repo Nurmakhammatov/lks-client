25df2d208b8b1c93e43d27f23787764cf6fd3b30
Shukhratullo <shuhratillon@gmail.com>
2024-02-08 17:25:52 +0500

    Mocked up taskbar

---
+++ components/system/TaskbarEntries.tsx
@@ -0,0 +1,12 @@
+import TaskbarEntry from 'components/system/TaskbarEntry';
+import StyledTaskbarEntries from 'styles/components/system/StyledTaskbarEntries';
+
+// TODO: TaskbarEntry
+
+const TaskbarEntries: React.FC = () => (
+  <StyledTaskbarEntries>
+    <TaskbarEntry />
+  </StyledTaskbarEntries>
+);
+
+export default TaskbarEntries;
---
+++ styles/components/system/StyledTaskbarEntries.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledTaskbarEntries = styled.ol`
+  background-color: purple;
+  left: 30px;
+  height: 100%;
+  position: absolute;
+  right: 90px;
+  width: 100%;
+`;
+
+export default StyledTaskbarEntries;
