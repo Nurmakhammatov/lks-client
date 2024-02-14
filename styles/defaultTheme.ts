d442b6c77e75355236b9bb2a9f42e35690850874
Shukhratullo <shuhratillon@gmail.com>
2024-02-14 16:47:55 +0500

    Define defaultTheme as DefaultTheme type

--- styles/defaultTheme.ts
+++ styles/defaultTheme.ts
@@ -1,3 +1,4 @@
+import type { DefaultTheme } from 'styled-components';
 import vantaWaves from 'utils/vantaWaves';
 
 const colors = {
@@ -35,7 +36,7 @@ const wallpaper = vantaWaves({
   zoom: 0.9
 });
 
-const defaultTheme = {
+const defaultTheme: DefaultTheme = {
   colors,
   fonts,
   sizes,
